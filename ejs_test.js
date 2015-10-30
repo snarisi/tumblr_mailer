2e308a5acd0e2dda486768df43132714cc1f9d66
Sam Narisi <snarisi@gmail.com>
2015-10-30 11:53:51 -0400

    Initial commit

--- ejs_test.js
+++
@@ -1,6 +0,0 @@
-var ejs = require('ejs');
-
-var template = 'Hi, my name is <%= name %>. The best movie ever is <%= favoriteMovie %>.'
-
-var renderedText = ejs.render(template, { name: 'Alice Cooper', favoriteMovie: 'Welcome to My Nightmare'});
-console.log(renderedText);
